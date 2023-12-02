3e402d49e8b09fe9fd7d16fa5be697f05edebb2a
asita26 <asita98890@gmail.com>
2023-12-02 20:20:34 +0530

    Refactoring code to insert node at end

--- LinkedListProgram.java
+++ LinkedListProgram.java
@@ -1,28 +1,24 @@
 // Main.java
 
-// Node class to represent each element in the linked list
 class Node {
     int data;
     Node next;
 
-    // Constructor to initialize a node with data
     public Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
 
-// LinkedList class to represent the linked list and perform operations
 class LinkedList {
-    Node head; // Head of the linked list
+    Node head;
 
-    // Constructor to initialize an empty linked list
     public LinkedList() {
         this.head = null;
     }
 
     // Method to insert a new node at the end of the linked list
-    public void insert(int data) {
+    public void insertAtEnd(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
@@ -39,25 +35,24 @@ class LinkedList {
     public void display() {
         Node current = head;
         while (current != null) {
-            System.out.print(current.data + " ");
+            System.out.print(current.data + "->");
             current = current.next;
         }
-        System.out.println();
+        System.out.println("null");
     }
 }
 
-// Main class to test the linked list
 public class LinkedListProgram {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
 
-        // Inserting elements into the linked list
-        linkedList.insert(56);
-        linkedList.insert(30);
-        linkedList.insert(70);
+        // Appending nodes to create the specified linked list sequence
+        linkedList.insertAtEnd(56);  // Adding 56
+        linkedList.insertAtEnd(30);  // Appending 30
+        linkedList.insertAtEnd(70);  // Appending 70
 
         // Displaying the elements of the linked list
-        System.out.print("Linked List: ");
+        System.out.print("LinkedList Sequence: ");
         linkedList.display();
     }
 }
