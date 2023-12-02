ecd8d189085a3905b41b19d8968b35af5e84bd4c
asita26 <asita98890@gmail.com>
2023-12-02 20:27:51 +0530

    Refactoring code to allow insertions between nodes

--- LinkedListProgram.java
+++ LinkedListProgram.java
@@ -17,17 +17,22 @@ class LinkedList {
         this.head = null;
     }
 
-    // Method to insert a new node at the end of the linked list
-    public void insertAtEnd(int data) {
+    // Method to insert a new node after a specific value
+    public void insertAfter(int previousValue, int data) {
         Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
+        Node current = head;
+
+        // Find the node with the previous value
+        while (current != null && current.data != previousValue) {
+            current = current.next;
+        }
+
+        // If the node is found, insert the new node after it
+        if (current != null) {
+            newNode.next = current.next;
             current.next = newNode;
+        } else {
+            System.out.println("Previous value " + previousValue + " not found in the linked list.");
         }
     }
 
@@ -46,13 +51,12 @@ public class LinkedListProgram {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
 
-        // Appending nodes to create the specified linked list sequence
-        linkedList.insertAtEnd(56);  // Adding 56
-        linkedList.insertAtEnd(30);  // Appending 30
-        linkedList.insertAtEnd(70);  // Appending 70
+        // Inserting elements into the linked list
+        linkedList.insertAfter(56, 30); // Inserting 30 after 56
+        linkedList.insertAfter(30, 70); // Inserting 70 after 30
 
         // Displaying the elements of the linked list
-        System.out.print("LinkedList Sequence: ");
+        System.out.print("Linked List: ");
         linkedList.display();
     }
 }
