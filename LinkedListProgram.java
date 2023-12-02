a6ba81f7d011a4a6adc9178ab8220034f4b638f5
asita26 <asita98890@gmail.com>
2023-12-02 21:45:45 +0530

    Adding functionality of UC10 in UC9

--- LinkedListProgram.java
+++ LinkedListProgram.java
@@ -1,88 +1,51 @@
 public class LinkedListProgram {
 
     // Node class to represent each element in the linked list
-    static class Node {
-        int data;
-        Node next;
+    static class Node<T extends Comparable<T>> {
+        T data;
+        Node<T> next;
 
         // Constructor to initialize a node with data
-        public Node(int data) {
+        public Node(T data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    // LinkedList class to represent the linked list and perform operations
-    static class LinkedList {
-        Node head;
+    // SortedLinkedList class to represent the ordered linked list
+    static class SortedLinkedList<T extends Comparable<T>> {
+        Node<T> head;
 
         // Constructor to initialize an empty linked list
-        public LinkedList() {
+        public SortedLinkedList() {
             this.head = null;
         }
 
-        // Method to insert a new node at the end of the linked list
-        public void insert(int data) {
-            Node newNode = new Node(data);
-            if (head == null) {
+        // Method to insert a new node in sorted order
+        public void add(T data) {
+            Node<T> newNode = new Node<>(data);
+
+            // If the list is empty or the new node is smaller than the head
+            if (head == null || data.compareTo(head.data) < 0) {
+                newNode.next = head;
                 head = newNode;
-            } else {
-                Node current = head;
-                while (current.next != null) {
-                    current = current.next;
-                }
-                current.next = newNode;
+                return;
             }
-        }
 
-        // Method to search for a node with a specific value
-        public Node search(int key) {
-            Node current = head;
-            while (current != null) {
-                if (current.data == key) {
-                    return current;
-                }
-                current = current.next;
-            }
-            return null; // Return null if the key is not found
-        }
-
-        // Method to delete a node by value
-        public void delete(int key) {
-            Node current = head;
-            Node prev = null;
-
-            // Find the node to be deleted and its previous node
-            while (current != null && current.data != key) {
-                prev = current;
+            // Find the appropriate position to insert the new node
+            Node<T> current = head;
+            while (current.next != null && data.compareTo(current.next.data) > 0) {
                 current = current.next;
             }
 
-            // If the node with the key is found, delete it
-            if (current != null) {
-                // If the node to be deleted is the head
-                if (prev == null) {
-                    head = current.next;
-                } else {
-                    prev.next = current.next;
-                }
-            }
-        }
-
-        // Method to calculate the size of the linked list
-        public int size() {
-            int count = 0;
-            Node current = head;
-            while (current != null) {
-                count++;
-                current = current.next;
-            }
-            return count;
+            // Insert the new node
+            newNode.next = current.next;
+            current.next = newNode;
         }
 
         // Method to display the elements of the linked list
         public void display() {
-            Node current = head;
+            Node<T> current = head;
             while (current != null) {
                 System.out.print(current.data);
                 if (current.next != null) {
@@ -95,34 +58,15 @@ public class LinkedListProgram {
     }
 
     public static void main(String[] args) {
-        // Creating a linked list
-        LinkedList linkedList = new LinkedList();
-        linkedList.insert(56);
-        linkedList.insert(30);
-        linkedList.insert(40);
-        linkedList.insert(70);
+        // Creating a sorted linked list
+        SortedLinkedList<Integer> sortedLinkedList = new SortedLinkedList<>();
+        sortedLinkedList.add(56);
+        sortedLinkedList.add(30);
+        sortedLinkedList.add(40);
+        sortedLinkedList.add(70);
 
-        // Displaying the elements of the linked list before deletion
-        System.out.print("Linked List before deletion: ");
-        linkedList.display();
-
-        // Searching for a node with value 40
-        int keyToDelete = 40;
-        Node foundNode = linkedList.search(keyToDelete);
-
-        // Deleting the node with value 40
-        if (foundNode != null) {
-            linkedList.delete(keyToDelete);
-            System.out.println("Node with value " + keyToDelete + " deleted.");
-        } else {
-            System.out.println("Node with value " + keyToDelete + " not found.");
-        }
-
-        // Displaying the elements of the linked list after deletion
-        System.out.print("Linked List after deletion: ");
-        linkedList.display();
-
-        // Displaying the size of the linked list
-        System.out.println("Linked List size: " + linkedList.size());
+        // Displaying the elements of the sorted linked list
+        System.out.print("Sorted Linked List: ");
+        sortedLinkedList.display();
     }
 }
